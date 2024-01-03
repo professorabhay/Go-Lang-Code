d97d7a20a8a39edbe1fc59b867d1a51a6e4c100e
professorabhay <abhayetwup123@gmail.com>
2024-01-03 15:51:17 +0530

    Created routes

--- basicAPI/main.go
+++ basicAPI/main.go
@@ -1,27 +1,66 @@
-package main 
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-// Modal for course - file 
-type Course struct{
-	CourseId string `json:"courseId"`
-	CourseName string `json:"courseName"`
-	CoursePrice int `json:"coursePrice"`
-	Author *Author `json:"author"` //Author Pointing to the custom type that I defined just below
+	"github.com/gorilla/mux"
+)
+
+// Modal for course - file
+type Course struct {
+	CourseId    string  `json:"courseId"`
+	CourseName  string  `json:"courseName"`
+	CoursePrice int     `json:"coursePrice"`
+	Author      *Author `json:"author"` //Author Pointing to the custom type that I defined just below
 }
 
-type Author struct{
+type Author struct {
 	Fullname string `json:"fullname"`
 	Website  string `json:"website"`
 }
 
-//fake DB 
+// fake DB
 var courses []Course
 
-// middleware, helper - file 
-func (c *Course) IsEmpty() bool{
-	return c.CourseId == "" && c.CourseName == "" 
+// middleware, helper - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
 }
 
-func main(){
+func main() {
 
+}
+
+// controllers - file
+
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to the home page</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses endpoint hit")
+	w.Header().Set("Content-Type", "applicatioan/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course endpoint hit")
+	w.Header().Set("Content-Type", "applicatioan/json")
+
+	// get the id from the url/request
+	params := mux.Vars(r)
+
+	// loop through the courses and find the one with the id from the params and return response
+	for _ , course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course found with given id")
+	return
 }
